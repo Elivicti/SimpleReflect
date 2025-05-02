1da3b16fa2daa87a846d053c08ba93c64aeee975
Elivicti <edwynb72@gmail.com>
2025-05-02 15:08:49 +0800

    add member_count, member_index and get_member; rewrite visit_member with get_member

--- include/SimpleReflect/Reflect.hpp
+++ include/SimpleReflect/Reflect.hpp
@@ -184,6 +184,18 @@ template<typename Func, typename Cls, typename MemberT, typename StringT = Strin
 concept for_each_member_invokable =
 	is_for_each_member_invokable_v<Func, Cls, StringT, MemberT>;
 
+
+template<reflectable Cls>
+struct member_count
+{
+	inline static constexpr std::size_t value =std::tuple_size_v<
+		decltype(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE)
+	>;
+};
+
+template<reflectable Cls>
+inline constexpr std::size_t member_count_v = member_count<Cls>::value;
+
 //////////////////////////////////////////////////////////////
 
 NAMESPACE_BEGIN(NS_DETAIL)
@@ -196,7 +208,7 @@ inline constexpr bool member_type_info_invokable = is_for_each_member_invokable<
 
 template<typename Cls, typename Func, typename ...MemberInfoT>
 	requires (member_type_info_invokable<Func, Cls, MemberInfoT> && ...)
-void for_each_member_impl_expand(Cls* ptr, Func&& func, MemberInfoT& ...pair)
+constexpr void for_each_member_impl_expand(Cls* ptr, Func&& func, MemberInfoT& ...pair)
 {
 	(std::invoke(
 		std::forward<Func>(func), ptr, pair.name, pair.to_real_variable(ptr)
@@ -204,7 +216,7 @@ void for_each_member_impl_expand(Cls* ptr, Func&& func, MemberInfoT& ...pair)
 }
 
 template<typename Cls, typename Func, std::size_t ...Indices>
-void for_each_member_impl(Cls* ptr, Func&& func, std::index_sequence<Indices...>)
+constexpr void for_each_member_impl(Cls* ptr, Func&& func, std::index_sequence<Indices...>)
 {
 	for_each_member_impl_expand(
 		ptr, std::forward<Func>(func),
@@ -213,11 +225,27 @@ void for_each_member_impl(Cls* ptr, Func&& func, std::index_sequence<Indices...>
 }
 
 template<typename Cls, typename Func, std::size_t ...Indices>
-void visit_member_impl(Func&& func, std::index_sequence<Indices...>)
+constexpr void visit_member_impl(Func&& func, std::index_sequence<Indices...>)
 {
 	(std::invoke(
 		std::forward<Func>(func), std::get<Indices>(MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE)
 	), ...);
+}
+
+
+template<reflectable Cls, std::size_t Index>
+using InfoTupleElem = std::remove_cvref_t<
+	decltype(std::get<Index>(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE))
+>;
+
+template<reflectable Cls, StaticString Name, std::size_t ...Indices>
+constexpr std::size_t member_index_impl(std::index_sequence<Indices...>)
+{
+	std::size_t result = (std::size_t)-1;
+    ((InfoTupleElem<Cls, Indices>::name == Name
+		? (result = Indices) : 0
+	), ...);
+    return result;
 }
 
 NAMESPACE_END(NS_DETAIL)
@@ -226,12 +254,29 @@ NAMESPACE_END(NS_DETAIL)
 template<reflectable Cls, typename Func>
 constexpr void for_each_member(Cls* ptr, Func&& func)
 {
-	using TupleIndex = std::make_index_sequence<
-		std::tuple_size_v<decltype(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE)>
-	>;
+	using TupleIndex = std::make_index_sequence<member_count_v<Cls>>;
 	NS_DETAIL::for_each_member_impl(ptr, std::forward<Func>(func), TupleIndex{});
 }
 
+template<reflectable Cls, StaticString Name>
+constexpr std::make_signed_t<std::size_t> member_index()
+{
+	using TupleIndex = std::make_index_sequence<member_count_v<Cls>>;
+	return NS_DETAIL::member_index_impl<Cls, Name>(TupleIndex{});
+}
+
+template<StaticString Name, reflectable Cls>
+	requires (member_index<Cls, Name>() < member_count_v<Cls>)
+constexpr auto& get_member(Cls* ptr)
+{
+	constexpr auto idx = member_index<Cls, Name>();
+	return std::get<idx>(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE).to_real_variable(ptr);
+}
+
+template<StaticString Name, reflectable Cls>
+	requires (member_index<Cls, Name>() < member_count_v<Cls>)
+constexpr auto& get_member(Cls& obj)
+{ return get_member<Name>(&obj); }
 
 // Because function parameters are always considered runtime variable, Func will be instantiate for all member(type)s
 // that it can be invoked, but is only invoked when name is equal.
@@ -239,11 +284,9 @@ constexpr void for_each_member(Cls* ptr, Func&& func)
 template<reflectable Cls, typename Func, typename StringT>
 constexpr void visit_member(Cls* ptr, const StringT& name, Func&& visitor)
 {
-	using TupleIndex = std::make_index_sequence<
-		std::tuple_size_v<decltype(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE)>
-	>;
+	using TupleIndex = std::make_index_sequence<member_count_v<Cls>>;
 	NS_DETAIL::visit_member_impl<Cls>(
-		[&name, visitor, ptr](auto& pair) {
+		[&name, visitor, ptr](auto& pair) constexpr {
 			using MemberType = std::decay_t<decltype(pair.to_real_variable(ptr))>;
 			if constexpr (std::is_invocable_v<Func, Cls*, MemberType&>)
 			{
@@ -257,29 +300,20 @@ constexpr void visit_member(Cls* ptr, const StringT& name, Func&& visitor)
 }
 // This is the static version of `visit_member`, Func will only be instantiate for member with
 // specified name.
-// If no member in Cls with specified name exists, this function does nothing (and will not cause compile error).
+// If no member in Cls with specified name exists, a compile error occurrs.
 template<StaticString Name, reflectable Cls, typename Func>
 constexpr void visit_member(Cls* ptr, Func&& visitor)
 {
-	using TupleIndex = std::make_index_sequence<
-		std::tuple_size_v<decltype(NS_DETAIL::MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE)>
-	>;
-	NS_DETAIL::visit_member_impl<Cls>(
-		[visitor, ptr](auto& pair) {
-			if constexpr (pair.name == Name) {
-				visitor(ptr, pair.to_real_variable(ptr));
-			}
-		},
-		TupleIndex{}
-	);
+	visitor(ptr, get_member<Name>(ptr));
 }
 
 NAMESPACE_BEGIN(NS_DETAIL)
 template<typename Cls, typename Container, std::size_t ...Indices>
 constexpr Container member_names_impl(std::index_sequence<Indices...>)
 {
-	constexpr auto extract_name = [](const auto& tuple) {
-		return typename Container::value_type{ tuple.name };
+	constexpr auto extract_name = [](const auto& info) constexpr {
+		using MemberInfoT = std::remove_cvref_t<decltype(info)>;
+		return typename Container::value_type{ MemberInfoT::name };
 	};
 	return { extract_name(std::get<Indices>(MemberInfoWrapperType<Cls>::MEMBER_TYPE_INFO_TUPLE))... };
 }
@@ -307,7 +341,7 @@ constexpr Container member_names([[maybe_unused]] Cls* ptr)
 // The name of each member will be cast to typename Container::value_type.
 // A helper function that can deduce Cls from parameter
 template<typename Container, typename Cls>
-constexpr Container member_names([[maybe_unused]] Cls& ptr)
+constexpr Container member_names([[maybe_unused]] Cls& obj)
 { return member_names<Cls, Container>(); }
 
 // Get all reflected member names from Cls, store them in Container.
@@ -321,7 +355,7 @@ constexpr Container<StringView> member_names([[maybe_unused]] Cls* ptr)
 // The name of each member will be cast to Reflect::StringView.
 // A helper function that can deduce Cls from parameter
 template<template<class> class Container, typename Cls>
-constexpr Container<StringView> member_names([[maybe_unused]] Cls& ptr)
+constexpr Container<StringView> member_names([[maybe_unused]] Cls& obj)
 { return member_names<Cls, Container<StringView>>(); }
 
 
